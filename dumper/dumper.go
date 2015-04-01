63c76bbaa77b693a25930a5525d6215911664127
Christoph Brand <ch.brand@scolterius.net>
2015-04-01 03:55:13 +0200

    parser: Summary is now being also added to the dumped excel file.

--- dumper/dumper.go
+++ dumper/dumper.go
@@ -39,6 +39,7 @@ func (self *Dumper) generate() {
     self.sheet = self.file.AddSheet("timedata")
     self.generateHeader()
     self.generateFromData()
+    self.generateSummary()
 }
 
 // generateHeader prints the header entry.
